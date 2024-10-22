3ddbb8448b6f8a6bbaf0c1857ee19d4cf4b06949
jhonatanjacome07 <jhonatanjacome07@gmail.com>
2024-10-22 12:26:38 -0500

    Update

--- src/components/pages/about.tsx
+++ src/components/pages/about.tsx
@@ -9,6 +9,9 @@ import {
   FaBook,
 } from "react-icons/fa";
 
+// Importa la imagen directamente
+import valoresImage from "@/assets/valores/valores.jpg";
+
 export default function About() {
   const [activeTestimonial, setActiveTestimonial] = useState(0);
 
@@ -31,10 +34,10 @@ export default function About() {
     <div className="min-h-screen bg-radial-gradient from-[#00DAFE] to-white">
       <main className="container mx-auto px-4 py-12">
         <section className="mb-12">
-          <h1 className="text-4xl  mb-6 text-center font-bold text-sky-900">
+          <h1 className="text-4xl mb-6 text-center font-bold text-sky-900">
             Sobre Nosotros
           </h1>
-          <p className="text-lg text-sky-700  mb-4">
+          <p className="text-lg text-sky-700 mb-4">
             Somos un apasionado grupo de desarrolladores que, en colaboración
             con NoCountry, hemos creado este sitio web para facilitar la
             adopción de mascotas y apoyar a los refugios en su noble labor.
@@ -46,12 +49,12 @@ export default function About() {
             Misión y Visión
           </h2>
           <div className="bg-white bg-opacity-50 p-6 rounded-lg shadow-lg border-2 border-[#00DAFE] hover:border-[#0097B2] transition duration-300 ease-in-out">
-            <p className="text-lg text-sky-700  mb-4">
+            <p className="text-lg text-sky-700 mb-4">
               <strong>Misión:</strong> Conectar a mascotas necesitadas con
               familias amorosas, utilizando tecnología innovadora para
               simplificar el proceso de adopción.
             </p>
-            <p className="text-lg text-sky-700  mb-4">
+            <p className="text-lg text-sky-700 mb-4">
               <strong>Visión:</strong> Crear un mundo donde cada mascota tenga
               un hogar y cada familia pueda experimentar la alegría de tener una
               mascota.
@@ -61,36 +64,34 @@ export default function About() {
 
         <section className="mb-12 flex items-center">
           <div className="w-2/3 pr-4">
-            {" "}
-            {/* Agregado pr-4 para espacio a la derecha */}
             <h2 className="text-3xl font-semibold text-sky-900 mb-4">
               Nuestros Valores
             </h2>
             <ul className="list-disc list-inside">
-              <li className="flex items-center text-lg text-sky-700  mb-4 ">
+              <li className="flex items-center text-lg text-sky-700 mb-4">
                 <FaPaw className="mr-2 text-[#00DAFE]" /> Compasión por los
                 animales
               </li>
-              <li className="flex items-center text-lg text-sky-700  mb-4 ">
+              <li className="flex items-center text-lg text-sky-700 mb-4">
                 <FaLightbulb className="mr-2 text-[#00DAFE]" /> Innovación
                 tecnológica
               </li>
-              <li className="flex items-center text-lg text-sky-700  mb-4 ">
+              <li className="flex items-center text-lg text-sky-700 mb-4">
                 <FaEye className="mr-2 text-[#00DAFE]" /> Transparencia en
                 nuestras operaciones
               </li>
-              <li className="flex items-center text-lg text-sky-700  mb-4 ">
+              <li className="flex items-center text-lg text-sky-700 mb-4">
                 <FaHandsHelping className="mr-2 text-[#00DAFE]" /> Colaboración
                 con refugios y comunidades
               </li>
-              <li className="flex items-center text-lg text-sky-700  mb-4 ">
+              <li className="flex items-center text-lg text-sky-700 mb-4">
                 <FaBook className="mr-2 text-[#00DAFE]" /> Educación sobre la
                 tenencia responsable de mascotas
               </li>
             </ul>
           </div>
           <motion.img
-            src="/src/assets/valores/valores.jpg"
+            src={valoresImage} // Usa la imagen importada
             alt="Valores"
             className="w-1/3 rounded-lg shadow-lg w-full max-w-[300px] ml-auto"
             whileHover={{ scale: 1.05 }}
@@ -99,14 +100,14 @@ export default function About() {
         </section>
 
         <section className="mb-12">
-          <h2 className="text-3xl font-semibold font-semibold text-sky-900 mb-4">
+          <h2 className="text-3xl font-semibold text-sky-900 mb-4">
             Testimonios
           </h2>
           <div className="bg-white bg-opacity-50 p-6 rounded-lg shadow-lg border-2 border-[#00DAFE] hover:border-[#0097B2] transition duration-300 ease-in-out">
-            <p className="mb-4 italic text-center text-lg text-sky-700  mb-4 ">
+            <p className="mb-4 italic text-center text-lg text-sky-700 mb-4">
               "{testimonials[activeTestimonial].text}"
             </p>
-            <p className="font-semibold text-center text-lg text-sky-700  mb-4">
+            <p className="font-semibold text-center text-lg text-sky-700 mb-4">
               - {testimonials[activeTestimonial].name}
             </p>
             <div className="mt-4 flex justify-center space-x-2">
@@ -127,7 +128,7 @@ export default function About() {
           <h2 className="text-3xl font-semibold font-semibold text-sky-900 mb-4">
             Compromiso Social
           </h2>
-          <p className="text-lg text-sky-700  mb-4">
+          <p className="text-lg text-sky-700 mb-4">
             Además de facilitar adopciones, trabajamos activamente en programas
             de educación sobre tenencia responsable de mascotas y colaboramos
             con refugios locales para mejorar las condiciones de vida de los
